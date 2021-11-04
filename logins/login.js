af3d7e15556a8b3715ce6ef0e3e9ae60d5f9e235
Rrocha20 <91967625+Rrocha20@users.noreply.github.com>
2021-11-04 17:25:54 +0100

    Add files via upload
    avance login

--- logins/login.js
+++ logins/login.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity, Alert, } from "react-native";
+
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.Title}>hola mundo</Text>
+
       <Image
-          source ={{uri: 'https://picsum.photos/200/300'}}
+          source ={{uri: 'https://i2.wp.com/imagenesdejardines.com/wp-content/uploads/2016/05/Bonitas-imagenes-de-flores-para-enviar-por-mensajes-de-whatsapp.jpg?resize=530%2C795&ssl=1'}}
           style={styles.image}
       />
+      <Text style={styles.Title}>Hola Mundo</Text>
+      <TouchableOpacity
+        onPress={ () => Alert.alert('Hello!!')}
+        style= {styles.button}
+      >
+        <Text style={styles.buttonText}>Press me</Text>
+          
+      </TouchableOpacity>
+
     </View>
   );
 };
@@ -18,9 +28,17 @@ const styles = StyleSheet.create({
      flex: 1,
      justifyContent: 'center', 
      alignItems: "center", 
-     backgroundColor: '#292929' },
-  Title: {fontSize: 30, color: 'red' },
-  image: {height: 200, width: 200}
+     backgroundColor: '#303030' },
+  Title: {fontSize: 30, color: 'white' },
+  image: {height: 250, width: 250, borderRadius: 130},
+  button:{
+    backgroundColor: 'blue',
+    padding: 7,
+    marginTop: 10
+  },
+  buttonText: {
+    color: '#fff'
+  }
 
 });
 
