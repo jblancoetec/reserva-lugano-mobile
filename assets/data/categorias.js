d578d9d7c97434acfd55116ddd4d755964ed541e
SebaX18 <89862738+SebaX18@users.noreply.github.com>
2021-11-06 12:48:39 -0300

    Add files via upload
    Informacion para la pantalla principal

---
+++ assets/data/categorias.js
@@ -0,0 +1,30 @@
+export default {
+  items: [
+    {
+      id: "category1",
+      title: "Mas buscados",
+      plantas: [
+        {
+          id: "planta1",
+          plantaimg: 'https://source.unsplash.com/random',
+        },
+        {
+          id: "planta2",
+          plantaimg: 'https://source.unsplash.com/random',
+        },
+        {
+          id: "planta3",
+          plantaimg: 'https://source.unsplash.com/random',
+        },
+        {
+          id: "planta4",
+          plantaimg: 'https://source.unsplash.com/random',
+        },
+        {
+          id: "planta5",
+          plantaimg: 'https://source.unsplash.com/random',
+        },
+      ],
+    },
+  ],
+};
