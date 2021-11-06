7e32262b7ee04964e4b2bf9f308e21e2ed667074
SebaX18 <89862738+SebaX18@users.noreply.github.com>
2021-11-06 12:49:22 -0300

    Add files via upload
    Prueba de la navegacion

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Index from "./src/index"
 import Login from "./logins/login"
+import Main from './MainScreen/Main';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StatusBar} from 'react-native';
+import {Entypo,FontAwesome} from '@expo/vector-icons'
+const Tab = createBottomTabNavigator();
 
-export default function App() {
+function MyTabs() {
   return (
-    <Login/>
+    <Tab.Navigator style={{paddingTop:StatusBar.currentHeight}}>
+      <Tab.Screen 
+        options = { { title : ()=><Entypo name="menu" size = {24} />}}
+        name="home" 
+        component={Login} 
+      />
+
+      <Tab.Screen
+        options ={{title:()=><FontAwesome name="search" size={24} color="black" />}}
+        name="Buscar" 
+        component={Main} 
+       />
+    </Tab.Navigator>
   );
 }
 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
