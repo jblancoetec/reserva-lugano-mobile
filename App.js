2b7f28b6cdd174073ff958082c4206156661653b
Joaquin Blanco <jblanco@etec.uba.ar>
2021-11-23 01:41:13 -0300

    resolviendo algunos errores de dependencias

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import Index from "./src/index"
-import Login from "./logins/login"
-import Main from './MainScreen/Main';
-import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {StatusBar} from 'react-native';
-import {Entypo,FontAwesome} from '@expo/vector-icons'
-const Tab = createBottomTabNavigator();
+import React from "react";
+import Login from "./logins/login";
+import Main from "./MainScreen/Main";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar } from "react-native";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
 
+// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
-    <Tab.Navigator style={{paddingTop:StatusBar.currentHeight}}>
-      <Tab.Screen 
-        options = { { title : ()=><Entypo name="menu" size = {24} />}}
-        name="home" 
-        component={Login} 
+    <Tab.Navigator style={{ paddingTop: StatusBar.currentHeight }}>
+      <Tab.Screen
+        options={{ title: () => <Entypo name="menu" size={24} /> }}
+        name="home"
+        component={Login}
       />
 
       <Tab.Screen
-        options ={{title:()=><FontAwesome name="search" size={24} color="black" />}}
-        name="Buscar" 
-        component={Main} 
-       />
+        options={{
+          title: () => <FontAwesome name="search" size={24} color="black" />,
+        }}
+        name="Buscar"
+        component={Main}
+      />
     </Tab.Navigator>
   );
 }
